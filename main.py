0614ce175a1b6124c45b0947910c29868e0859cc
luciekasperczyk <74659557+luciekasperczyk@users.noreply.github.com>
2022-10-27 11:28:27 +0200

    collect all the coordinates into a csv file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import csv
+
 import cv2
+import keyboard as keyboard
 import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
@@ -6,12 +9,19 @@ cap = cv2.VideoCapture(0)
 mediapipeHands = mp.solutions.hands
 hands = mediapipeHands.Hands()
 mediapipeDrawing = mp.solutions.drawing_utils
+coordinates = []
+for i in range (21) :
+    coordinates.append("x"+str(i))
+    coordinates.append("y"+str(i))
+    coordinates.append("z"+str(i))
 
 while True:
     ret, img = cap.read() #create and open the camera window
     img = cv2.flip(img, 1) #flip the image so it is like a miror
     #create a list of all the different fingertips from 0 to 20
     fingerTips = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    #create coordinates for the csv file
+
     #create an empty list for the future hand's landmarks
     landMarkList = []
     results = hands.process(img)
@@ -26,16 +36,23 @@ while True:
         #for each landmark add it to the list and create and id
         for id, landMark in enumerate(hand_landmark.landmark):
             landMarkList.append(landMark)
-        #for each fingertip collect the coordinates x, y, z
-        for tip in fingerTips:
-            x, y, z = int(landMarkList[tip].x), int(landMarkList[tip].y), int(landMarkList[tip].z)
-        #print the coordinates for each fingertip/landmark
-        for id in enumerate(hand_landmark.landmark):
-            print(id, ":", x, y, z)
-        #create a method to add all the coordinates in a csv file
-            #when a selected key from the key board is pushed, take a picture of the hand
-            #create a csv file that is going to collect all the coordinates from the taken picture
-            #/!\ don't forget to write down all the sources you use !
+
+        if keyboard.is_pressed("l"):
+            # print the coordinates for each fingertip/landmark
+            data = []
+            for id in hand_landmark.landmark:
+                data.append({
+                    id.x,
+                    id.y,
+                    id.z,
+                })
+            print(data)
+            #open the file in the write mode
+            with open('./data.csv', 'a') as file :
+                #create the csv writer
+                writer = csv.writer(file)
+                #write rows in the csv file
+                writer.writerow(data)
 
     cv2.imshow("Hand Tracking", img)
     cv2.waitKey(1)
